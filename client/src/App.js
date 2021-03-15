e525e530959a3c69f71a74fbe97ffc1b7576c324
Lucky Jaiswal <jaiswal.lucky8888@gmail.com>
2021-03-16 10:18:18 +1100

    Initialised react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Face Mask Check Application build in progress....</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
