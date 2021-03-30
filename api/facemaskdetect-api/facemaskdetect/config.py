9b0ea501e575f786472a310c072e25cc545e476e
vats16 <piyushvats@gmail.com>
2021-03-30 22:09:03 +1100

    User models + rds connector + register/login api endpoints
    User model initialised
    rds connected
    rds initialised
    register/login endpoints

--- api/facemaskdetect-api/facemaskdetect/config.py
+++ api/facemaskdetect-api/facemaskdetect/config.py
@@ -6,7 +6,7 @@
 
 class BaseConfig(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = 'mysql://wholesal_ss3b:ZkEjU5uFR7z7@115.70.228.70:3306/wholesal_ss3b'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SECRET_KEY = ""
+    SECRET_KEY = "dd02dbe50eb41792067d9d650cd3ba58df0c90c6466ccea7"
--- api/facemaskdetect-api/facemaskdetect/models.py
+++ api/facemaskdetect-api/facemaskdetect/models.py
@@ -4,50 +4,83 @@ models.py
 """
 
 from datetime import datetime
+from dateutil import parser
+from flask import current_app
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.mysql import INTEGER
 from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
+from .services.misc import datetime_to_str, parse_datetime
+import jwt
 
 db = SQLAlchemy()
+
+required_fields = {'users':['student_id', 'is_authority' 'first_name', 'last_name', 'password']}
 
 
 class User(db.Model):
     __tablename__ = 'users'
 
     student_id = db.Column(db.Integer, primary_key=True)
-    is_admin = db.Column(db.Integer)
+    is_authority = db.Column(db.Integer)
     first_name = db.Column(db.String(191), nullable=False)
     last_name = db.Column(db.String(191), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    confirm_admin = db.Column(db.String(255), nullable=True)
     created_date = db.Column(db.DateTime, default=datetime.utcnow)
     updated_date = db.Column(db.DateTime, default=datetime.utcnow)
 
-    circuits = relationship("Circuit")
 
-    def __init__(self, student_id, is_admin, first_name, last_name, email, password, confirm_admin):
+    def __init__(self, student_id, is_authority, first_name, last_name, email, password):
         self.student_id = student_id
-        self.is_admin = is_admin
+        self.is_authority = is_authority
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = generate_password_hash(password, method='sha256')
-        self.confirm_admin = generate_password_hash(confirm_admin, method='sha256')
+    
     
     @classmethod
     def authenticate(cls, **kwargs):
-        email = kwargs.get('email')
+        student_id = kwargs.get('student_id')
         password = kwargs.get('password')
         
-        if not email or not password:
+        if not student_id or not password:
             return None
 
-        user = cls.query.filter_by(email=email).first()
+        user = cls.query.filter_by(student_id=student_id).first()
         if not user or not check_password_hash(user.password, password):
             return None
 
         return user
 
+    @classmethod
+    def decode_auth_token(cls, token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(token, current_app.config['SECRET_KEY'])
+            return payload['sub']
+        except Exception:
+            return 'Invalid token. Please log in again.'
+
     def to_dict(self):
-        return dict(id=self.student_id, email=self.email)
+        return {
+            'student_id':self.student_id,
+            'email':self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'is_authority': self.is_authority
+        }
+
+
+
+
+
+
+
+
+
