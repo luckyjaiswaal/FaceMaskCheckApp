64fc2a6fb02677a05a72f80d21e9716346e1c1f7
vats16 <piyushvats@gmail.com>
2021-04-24 19:21:25 +1000

    update login endpoint to return userid
    updated login endpoint to return user_id

--- api/facemaskdetect-api/facemaskdetect/models.py
+++ api/facemaskdetect-api/facemaskdetect/models.py
@@ -72,6 +72,7 @@ class User(db.Model):
 
     def to_dict(self):
         return {
+            'user_id' : self.user_id,
             'email':self.email,
             'first_name': self.first_name,
             'last_name': self.last_name
