8a99960a93b0818e286aaf248b5ab83eef50b1c9
mathuchock <69452624+mathuchock@users.noreply.github.com>
2021-04-25 22:36:42 +1000

    Worked on the Add venue form
    Can someone please have a look to see if it works or looks alright? I couldn't see it cause some errors

---
+++ client/src/pages/addNewVenue.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import '../style/style.css';
+import signup from '../components/facemaskcheck.jpg'
+import { withRouter } from "react-router-dom";
+import api from '../api'
+import { Link } from 'react-router-dom'
+import { Component } from "react";
+
+class addNewVenue extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            isLoading: false,
+        }
+    }
+
+    render(){
+        return (
+            <div className= "venueContainer">
+                <div className= "addNewVenue">Add New Venue</div>
+                <form action="#">
+                    <div className="user-details">
+                        <div className="input-box">
+                            <span className= "details">Venue name</span>
+                            <i nput type="text" placeholder="Enter Venue name" required/>
+                        </div>
+                        <div className="input-box">
+                            <span className= "details">Venue capacity</span>
+                            <input type="text" placeholder="Enter the venue capacity" required/>
+                        </div>
+                        <div className="input-box">
+                            <span className= "details">Authority name</span>
+                            <input type="text" placeholder="Enter the authority name" required/>
+                        </div>
+                        <div className="input-box">
+                            <span className= "details">Authority Contact number</span>
+                            <input type="text" placeholder="Enter the authority's contact number" required/>
+                        </div>
+                        <div className="input-box">
+                            <span className= "details">Authorisation code</span>
+                            <input type="text" placeholder="Enter the authorisation code" required/>
+                        </div>
+                        <div className="input-box">
+                            <span className= "details">Confirm Authorisation code</span>
+                            <input type="text" placeholder="Confirm authorisation code" required/>
+                        </div>
+                    </div>
+                    <div className="button">
+                        <input type="submit" value="Create venue"/>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+export default withRouter(addNewVenue);
