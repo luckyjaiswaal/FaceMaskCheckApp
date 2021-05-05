3a4a549d91b9e275c7a3caa7c69d7b83405501d4
pranay <pranay.khurana99@gmail.com>
2021-05-05 15:06:31 +1000

    added new chart to analytics dashboard

--- client/src/pages/Analytics.jsx
+++ client/src/pages/Analytics.jsx
@@ -5,6 +5,7 @@ import api from '../api'
 import { Link } from 'react-router-dom'
 import { Component } from "react";
 import Webcam from "react-webcam";
+import { ResponsivePie } from '@nivo/pie'
 
 
 class Analytics extends Component {
@@ -32,18 +33,166 @@ class Analytics extends Component {
      //else{
        //maskFound = "Mask was detected. Check in Successful."
      //}
-     api.get_venue_stats({venue_id}).then(res =>{
+     api.analytics({venue_id}).then(res =>{
        console.log(res.data)
      })
     
 
    };
 
+   
   render(){
+    let data1 = [
+      {
+        "id": "hack",
+        "label": "hack",
+        "value": 515,
+        "color": "hsl(44, 70%, 50%)"
+      },
+      {
+        "id": "go",
+        "label": "go",
+        "value": 469,
+        "color": "hsl(296, 70%, 50%)"
+      },
+      {
+        "id": "haskell",
+        "label": "haskell",
+        "value": 400,
+        "color": "hsl(100, 70%, 50%)"
+      },
+      {
+        "id": "erlang",
+        "label": "erlang",
+        "value": 547,
+        "color": "hsl(265, 70%, 50%)"
+      },
+      {
+        "id": "make",
+        "label": "make",
+        "value": 390,
+        "color": "hsl(99, 70%, 50%)"
+      }
+    ]
+    
+    const MyResponsivePie = ({ data1 /* see data tab */ }) => (
+      <ResponsivePie
+          data={data1}
+          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+          innerRadius={0.5}
+          padAngle={0.7}
+          cornerRadius={3}
+          activeOuterRadiusOffset={8}
+          borderWidth={1}
+          borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+          arcLinkLabelsSkipAngle={10}
+          arcLinkLabelsTextColor="#333333"
+          arcLinkLabelsThickness={2}
+          arcLinkLabelsColor={{ from: 'color' }}
+          arcLabelsSkipAngle={10}
+          arcLabelsTextColor={{ from: 'color', modifiers: [ [ 'darker', 2 ] ] }}
+          defs={[
+              {
+                  id: 'dots',
+                  type: 'patternDots',
+                  background: 'inherit',
+                  color: 'rgba(255, 255, 255, 0.3)',
+                  size: 4,
+                  padding: 1,
+                  stagger: true
+              },
+              {
+                  id: 'lines',
+                  type: 'patternLines',
+                  background: 'inherit',
+                  color: 'rgba(255, 255, 255, 0.3)',
+                  rotation: -45,
+                  lineWidth: 6,
+                  spacing: 10
+              }
+          ]}
+          fill={[
+              {
+                  match: {
+                      id: 'ruby'
+                  },
+                  id: 'dots'
+              },
+              {
+                  match: {
+                      id: 'c'
+                  },
+                  id: 'dots'
+              },
+              {
+                  match: {
+                      id: 'go'
+                  },
+                  id: 'dots'
+              },
+              {
+                  match: {
+                      id: 'python'
+                  },
+                  id: 'dots'
+              },
+              {
+                  match: {
+                      id: 'scala'
+                  },
+                  id: 'lines'
+              },
+              {
+                  match: {
+                      id: 'lisp'
+                  },
+                  id: 'lines'
+              },
+              {
+                  match: {
+                      id: 'elixir'
+                  },
+                  id: 'lines'
+              },
+              {
+                  match: {
+                      id: 'javascript'
+                  },
+                  id: 'lines'
+              }
+          ]}
+          legends={[
+              {
+                  anchor: 'bottom',
+                  direction: 'row',
+                  justify: false,
+                  translateX: 0,
+                  translateY: 56,
+                  itemsSpacing: 0,
+                  itemWidth: 100,
+                  itemHeight: 18,
+                  itemTextColor: '#999',
+                  itemDirection: 'left-to-right',
+                  itemOpacity: 1,
+                  symbolSize: 18,
+                  symbolShape: 'circle',
+                  effects: [
+                      {
+                          on: 'hover',
+                          style: {
+                              itemTextColor: '#000'
+                          }
+                      }
+                  ]
+              }
+          ]}
+      />
+  )
     return (
       <div className="VenueDashboard row">
         <div className="column2" key = "column2">
-          
+          <h1> CHarts</h1>
+          <MyResponsivePie>hi</MyResponsivePie>
         </div>
       </div>
     );
