36e18e0fd93bf1d22b9cf5058ed14aa7690fcfa1
pranay <pranay.khurana99@gmail.com>
2021-05-12 13:56:18 +1000

    pie chart for mask check stats

--- client/src/pages/Analytics.jsx
+++ client/src/pages/Analytics.jsx
@@ -5,7 +5,10 @@ import api from '../api'
 import { Link } from 'react-router-dom'
 import { Component } from "react";
 import Webcam from "react-webcam";
-import { ResponsivePie } from '@nivo/pie'
+import './venue_1.json';
+import { PureComponent } from 'react';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer, LabelList } from 'recharts';
+
 
 
 class Analytics extends Component {
@@ -15,10 +18,31 @@ class Analytics extends Component {
     this.state = {
       data: [],
       venue_id: localStorage.getItem('venue_id'),
+      facemask_successful_count: 0,
+      total_visitors : 0,
+      facemask_unsuccessful_count: 0,
     }
   }
 
-
+  CountStats = () => {
+    let venueJSON = require('./venue_1.json');
+    let len = venueJSON.venue_stats.length;
+    this.state.total_visitors = len;
+    let a = 0;
+    let b = 0;
+    const tempList = [];
+    for(let i = 0; i<len; i++){
+      if(venueJSON.venue_stats[i].facemask_check == 1){
+        a++
+      }
+      if(venueJSON.venue_stats[i].facemask_check == 0){
+        b++
+      }
+      tempList[i] = (venueJSON.venue_stats[i].visitor_name, venueJSON.venue_stats[i].visitor_temp)
+    }
+    this.state.facemask_successful_count = a
+    this.state.facemask_unsuccessful_count = b
+  };
 
    displayAnalytics = () => {
      
@@ -42,159 +66,25 @@ class Analytics extends Component {
 
    
   render(){
-    let data1 = [
-      {
-        "id": "hack",
-        "label": "hack",
-        "value": 515,
-        "color": "hsl(44, 70%, 50%)"
-      },
-      {
-        "id": "go",
-        "label": "go",
-        "value": 469,
-        "color": "hsl(296, 70%, 50%)"
-      },
-      {
-        "id": "haskell",
-        "label": "haskell",
-        "value": 400,
-        "color": "hsl(100, 70%, 50%)"
-      },
-      {
-        "id": "erlang",
-        "label": "erlang",
-        "value": 547,
-        "color": "hsl(265, 70%, 50%)"
-      },
-      {
-        "id": "make",
-        "label": "make",
-        "value": 390,
-        "color": "hsl(99, 70%, 50%)"
-      }
-    ]
-    
-    const MyResponsivePie = ({ data1 /* see data tab */ }) => (
-      <ResponsivePie
-          data={data1}
-          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-          innerRadius={0.5}
-          padAngle={0.7}
-          cornerRadius={3}
-          activeOuterRadiusOffset={8}
-          borderWidth={1}
-          borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
-          arcLinkLabelsSkipAngle={10}
-          arcLinkLabelsTextColor="#333333"
-          arcLinkLabelsThickness={2}
-          arcLinkLabelsColor={{ from: 'color' }}
-          arcLabelsSkipAngle={10}
-          arcLabelsTextColor={{ from: 'color', modifiers: [ [ 'darker', 2 ] ] }}
-          defs={[
-              {
-                  id: 'dots',
-                  type: 'patternDots',
-                  background: 'inherit',
-                  color: 'rgba(255, 255, 255, 0.3)',
-                  size: 4,
-                  padding: 1,
-                  stagger: true
-              },
-              {
-                  id: 'lines',
-                  type: 'patternLines',
-                  background: 'inherit',
-                  color: 'rgba(255, 255, 255, 0.3)',
-                  rotation: -45,
-                  lineWidth: 6,
-                  spacing: 10
-              }
-          ]}
-          fill={[
-              {
-                  match: {
-                      id: 'ruby'
-                  },
-                  id: 'dots'
-              },
-              {
-                  match: {
-                      id: 'c'
-                  },
-                  id: 'dots'
-              },
-              {
-                  match: {
-                      id: 'go'
-                  },
-                  id: 'dots'
-              },
-              {
-                  match: {
-                      id: 'python'
-                  },
-                  id: 'dots'
-              },
-              {
-                  match: {
-                      id: 'scala'
-                  },
-                  id: 'lines'
-              },
-              {
-                  match: {
-                      id: 'lisp'
-                  },
-                  id: 'lines'
-              },
-              {
-                  match: {
-                      id: 'elixir'
-                  },
-                  id: 'lines'
-              },
-              {
-                  match: {
-                      id: 'javascript'
-                  },
-                  id: 'lines'
-              }
-          ]}
-          legends={[
-              {
-                  anchor: 'bottom',
-                  direction: 'row',
-                  justify: false,
-                  translateX: 0,
-                  translateY: 56,
-                  itemsSpacing: 0,
-                  itemWidth: 100,
-                  itemHeight: 18,
-                  itemTextColor: '#999',
-                  itemDirection: 'left-to-right',
-                  itemOpacity: 1,
-                  symbolSize: 18,
-                  symbolShape: 'circle',
-                  effects: [
-                      {
-                          on: 'hover',
-                          style: {
-                              itemTextColor: '#000'
-                          }
-                      }
-                  ]
-              }
-          ]}
-      />
-  )
+    this.CountStats();
+    const data = [
+      {"name": "Successful FaceMask Check", value: this.state.facemask_successful_count},
+      {"name": "Unsuccessful FaceMask Check", value: this.state.facemask_unsuccessful_count}
+  ];
     return (
-      <div className="VenueDashboard row">
+      <div className="VenueDashboard row"> 
         <div className="column2" key = "column2">
           <h1> CHarts</h1>
-          <MyResponsivePie>hi</MyResponsivePie>
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={400} height={400}>
+              <Pie data={data} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#82ca9d" label>
+                <LabelList datakey = "name"/>
+                </Pie>
+            </PieChart>
+          </ResponsiveContainer>
         </div>
       </div>
+
     );
   }
 
