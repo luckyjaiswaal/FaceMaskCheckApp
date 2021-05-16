69363934aed639ddcde21dc0d31b50e6c5970a77
pranay <pranay.khurana99@gmail.com>
2021-05-16 19:37:38 +1000

    Added line charts for 7 day period

--- client/src/pages/Analytics.jsx
+++ client/src/pages/Analytics.jsx
@@ -8,8 +8,14 @@ import Webcam from "react-webcam";
 import './venue_1.json';
 import { PureComponent } from 'react';
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer, LabelList } from 'recharts';
-
-
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 class Analytics extends Component {
 
@@ -21,6 +27,8 @@ class Analytics extends Component {
       facemask_successful_count: 0,
       total_visitors : 0,
       facemask_unsuccessful_count: 0,
+      temperatureList: [],
+      personList: []
     }
   }
 
@@ -30,7 +38,6 @@ class Analytics extends Component {
     this.state.total_visitors = len;
     let a = 0;
     let b = 0;
-    const tempList = [];
     for(let i = 0; i<len; i++){
       if(venueJSON.venue_stats[i].facemask_check == 1){
         a++
@@ -38,25 +45,18 @@ class Analytics extends Component {
       if(venueJSON.venue_stats[i].facemask_check == 0){
         b++
       }
-      tempList[i] = (venueJSON.venue_stats[i].visitor_name, venueJSON.venue_stats[i].visitor_temp)
+      this.state.temperatureList[i] =  venueJSON.venue_stats[i].visitor_temp
+      this.state.personList[i] = venueJSON.venue_stats[i].visitor_name
     }
     this.state.facemask_successful_count = a
     this.state.facemask_unsuccessful_count = b
   };
 
+
    displayAnalytics = () => {
      
      const {venue_id} = this.state
-     const venue_info = [{venue_id}]
-     //how to call ai api.VenueStats({venue_info}).then(res =>{
-     //console.log(res.data) how to print data to console
-     //example of how to acces api returned data
-     //if (res.data['checkin_info'].check_in === 0){
-       //maskFound = "Mask not detected. Please try again."
-     //}
-     //else{
-       //maskFound = "Mask was detected. Check in Successful."
-     //}
+     const venue_info = [{venue_id}]  
      api.analytics({venue_id}).then(res =>{
        console.log(res.data)
      })
@@ -71,20 +71,220 @@ class Analytics extends Component {
       {"name": "Successful FaceMask Check", value: this.state.facemask_successful_count},
       {"name": "Unsuccessful FaceMask Check", value: this.state.facemask_unsuccessful_count}
   ];
+    const sevenday = [
+      {
+          "date": "9 Mar 2021",
+          "total_visitors": 50,
+          "successful_checkin": 29,
+          "unsuccessful_checkin": 21,
+          "facemask_fail": 15,
+          "temp_fail": 6
+      },
+      {
+          "date": "10 Mar 2021",
+          "total_visitors": 72,
+          "successful_checkin": 40,
+          "unsuccessful_checkin": 32,
+          "facemask_fail": 20,
+          "temp_fail": 12
+      },
+      {
+          "date": "11 Mar 2021",
+          "total_visitors": 30,
+          "successful_checkin": 25,
+          "unsuccessful_checkin": 5,
+          "facemask_fail": 3,
+          "temp_fail": 2
+      },
+      {
+          "date": "12 Mar 2021",
+          "total_visitors": 45,
+          "successful_checkin": 29,
+          "unsuccessful_checkin": 16,
+          "facemask_fail": 10,
+          "temp_fail": 6
+      },
+      {
+          "date": "13 Mar 2021",
+          "total_visitors": 64,
+          "successful_checkin": 40,
+          "unsuccessful_checkin": 24,
+          "facemask_fail": 15,
+          "temp_fail": 9
+      },
+      {
+          "date": "14 Mar 2021",
+          "total_visitors": 77,
+          "successful_checkin": 70,
+          "unsuccessful_checkin": 7,
+          "facemask_fail": 6,
+          "temp_fail": 1
+      },
+      {
+          "date": "15 Mar 2021",
+          "total_visitors": 67,
+          "successful_checkin": 55,
+          "unsuccessful_checkin": 12,
+          "facemask_fail": 8,
+          "temp_fail": 4
+      }
+  ];
+  
+  // const classes = useStyles();
+    console.log(this.state.temperatureList);
     return (
       <div className="VenueDashboard row"> 
         <div className="column2" key = "column2">
-          <h1> CHarts</h1>
-          <ResponsiveContainer width="100%" height="100%">
-            <PieChart width={400} height={400}>
-              <Pie data={data} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#82ca9d" label>
-                <LabelList datakey = "name"/>
+          <center>
+          <h1> Venue Analytics</h1>
+          </center>
+          
+          {/* <ResponsiveContainer width="30%" height="30%">
+            <PieChart width={600} height={600}>
+              <Pie data={data} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#82ca9d" stroke= "#000000" label>
+                <LabelList dataKey = "name" position="top" fill="	#000000"/>
                 </Pie>
             </PieChart>
           </ResponsiveContainer>
-        </div>
-      </div>
 
+            <ResponsiveContainer width="30%" height="30%">
+          <LineChart
+            width={200}
+            height={200}
+            data={sevenday}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="natural" dataKey="total_visitors" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="successful_checkin" stroke="#82ca9d" />
+            <Line type="monotone" dataKey="unsuccessful_checkin" stroke="#c76565"/>
+          </LineChart>
+          </ResponsiveContainer>
+
+          <ResponsiveContainer width="30%" height="30%">
+          <LineChart
+            width={500}
+            height={300}
+            data={sevenday}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="unsuccessful_checkin" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="facemask_fail" stroke="#82ca9d" />
+            <Line type="monotone" dataKey="temp_fail" stroke="#c76565"/>
+          </LineChart>
+          </ResponsiveContainer> */}
+          <div >
+      <Grid container spacing={3} style={{margin:30}}>
+        <Grid item xs={12} style={{height: 400}}>
+        <Card variant="outlined" >
+          <CardContent >
+            <center>
+            <Typography variant="h5" component="h2" gutterBottom>
+              Checkin Stats 
+            </Typography>
+
+            <PieChart  margin={{top: 0, right: 0, bottom: 0, left: 0}} width={400} height={400} >
+              <Pie data={data} dataKey="value"  outerRadius={60} fill="#82ca9d" stroke= "#000000" label>
+                <LabelList dataKey = "name" position="top" fill="	#000000"/>
+                </Pie>
+            </PieChart>
+            </center>
+          
+          </CardContent>
+          </Card>
+        </Grid>
+        
+      </Grid>
+      <Grid container spacing={3} style={{margin:30}}>
+      <Grid item xs={6} style={{height: 400}}>
+        <Card variant="outlined">
+          <CardContent>
+            <center>
+            <Typography variant="h5" component="h2" gutterBottom>
+              Checkin Stats 7 day period
+            </Typography>
+            <LineChart
+            width={400}
+            height={400}
+            data={sevenday}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="natural" dataKey="total_visitors" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="successful_checkin" stroke="#82ca9d" />
+            <Line type="monotone" dataKey="unsuccessful_checkin" stroke="#c76565"/>
+          </LineChart>
+            </center>
+         
+          </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6} style={{height: 400}}>
+        <Card variant="outlined">
+          <CardContent>
+            <center>
+            <Typography variant="h5" component="h2" gutterBottom>
+              Unsuccessful Checkin 7 day distribution
+            </Typography>
+            <LineChart
+            width={400}
+            height={400}
+            data={sevenday}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="unsuccessful_checkin" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="facemask_fail" stroke="#82ca9d" />
+            <Line type="monotone" dataKey="temp_fail" stroke="#c76565"/>
+          </LineChart>
+            </center>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+        </div>
+        
+      </div>
+      
+      
     );
   }
 
